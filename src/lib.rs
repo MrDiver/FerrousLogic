2ab16e0e30d1861ae99b64b8c589e103c38babfe
MrDiver <mrdiverlp@gmail.com>
2023-02-20 16:22:52 +0100

    chaos before something stupid

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,7 @@ use std::{
 };
 
 use bits::Bits;
-use tracing::{info, instrument};
+use tracing::{info, instrument, warn};
 pub mod bits;
 
 #[derive(Debug)]
@@ -46,8 +46,65 @@ impl ComponentManager {
         return id;
     }
 
-    fn accept_gate(&self, gate: Box<dyn Gate>) {
+    fn accept_gate(&mut self, gate: Box<dyn Gate>) -> usize {
+        let id = gate.get_id();
         self.gates.write().unwrap().insert(gate.get_id(), gate);
+        return id;
+    }
+
+    #[instrument(skip(self), ret)]
+    fn get_gate_inputs(&self, gate_id: &usize) -> Option<Vec<usize>> {
+        warn!("STOP USING THIS");
+        match self.gates.read().unwrap().get(gate_id) {
+            Some(gate) => Some(gate.gpio().in_pins.clone()),
+            None => None,
+        }
+    }
+
+    #[instrument(skip(self), ret)]
+    fn get_gate_outputs(&self, gate_id: &usize) -> Option<Vec<usize>> {
+        warn!("STOP USING THIS");
+        match self.gates.read().unwrap().get(gate_id) {
+            Some(gate) => Some(gate.gpio().out_pins.clone()),
+            None => None,
+        }
+    }
+
+    #[instrument(skip(self), ret)]
+    fn get_gate_inouts(&self, gate_id: &usize) -> Option<Vec<usize>> {
+        warn!("STOP USING THIS");
+        match self.gates.read().unwrap().get(gate_id) {
+            Some(gate) => Some(gate.gpio().inout_pins.clone()),
+            None => None,
+        }
+    }
+
+    fn accept_lump(&mut self, lump: Lump) -> usize {
+        let id = lump.id;
+        self.lumps.write().unwrap().insert(lump.id, lump);
+        return id;
+    }
+
+    fn connect_pin_to_lump(&mut self, pin_id: &usize, lump_id: &usize) {
+        let mut pins = self.pins.write().unwrap();
+        let pin = pins.get_mut(&pin_id);
+        let mut lumps = self.lumps.write().unwrap();
+        let lump = lumps.get_mut(&lump_id);
+        if let (Some(pin), Some(lump)) = (pin, lump) {
+            pin.connect(lump_id);
+            lump.connect(pin_id);
+        }
+    }
+
+    fn disconnect_pin_from_lump(&mut self, pin_id: &usize, lump_id: &usize) {
+        let mut pins = self.pins.write().unwrap();
+        let pin = pins.get_mut(&pin_id);
+        let mut lumps = self.lumps.write().unwrap();
+        let lump = lumps.get_mut(&lump_id);
+        if let (Some(pin), Some(lump)) = (pin, lump) {
+            pin.disconnect();
+            lump.disconnect(pin_id);
+        }
     }
 
     fn get_pin_value(&self, pin_id: &usize) -> Bits {
@@ -58,17 +115,6 @@ impl ComponentManager {
             .expect("The pin doesn´t exist")
             .value
             .clone()
-    }
-
-    #[instrument(skip(self))]
-    fn schedule_pin_update(&self, delay: u64, id: usize, value: Bits) {
-        let pue = PinUpdateEvent {
-            time: self.current_sim_time + delay,
-            target_pin_id: id,
-            value,
-        };
-        info!("Adding Pin Event at time {} for {}", pue.time, id);
-        self.pin_update_queue.write().unwrap().push(pue);
     }
 
     #[instrument(ret, skip(self))]
@@ -97,6 +143,31 @@ impl ComponentManager {
     }
 
     #[instrument(skip(self))]
+    fn process_lump_events(&mut self) {
+        let mut event_option = self.lump_update_queue.write().unwrap().pop_front();
+        while let Some(event) = &event_option {
+            info!("Updating Gate {}", event.target_lump_id);
+            self.lumps
+                .write()
+                .unwrap()
+                .get_mut(&event.target_lump_id)
+                .unwrap()
+                .accept_update(event, self);
+            event_option = self.lump_update_queue.write().unwrap().pop_front();
+        }
+    }
+
+    #[instrument(skip(self))]
+    fn schedule_pin_update(&self, delay: u64, id: usize, value: Bits) {
+        let pue = PinUpdateEvent {
+            time: self.current_sim_time + delay,
+            target_pin_id: id,
+            value,
+        };
+        info!("Adding Pin Event at time {} for {}", pue.time, id);
+        self.pin_update_queue.write().unwrap().push(pue);
+    }
+    #[instrument(skip(self))]
     pub fn process_pin_events(&mut self) {
         info!("Start Processing Events at time {}", self.current_sim_time);
         loop {
@@ -108,9 +179,9 @@ impl ComponentManager {
                     self.current_sim_time = event.time;
                     return;
                 }
-            }else{
+            } else {
                 // No Events
-                return
+                return;
             }
             let event = self.pin_update_queue.write().unwrap().pop().unwrap();
             // Processing Event
@@ -122,6 +193,7 @@ impl ComponentManager {
                 .accept_update(&event.value, self);
 
             self.process_gate_events();
+            self.process_lump_events();
         }
     }
 }
@@ -181,6 +253,14 @@ impl Pin {
             PinType::INOUT => todo!(),
         }
     }
+
+    fn connect(&mut self, lump_id: &usize) {
+        self.lump_id = Some(*lump_id);
+    }
+
+    fn disconnect(&mut self) {
+        self.lump_id = None;
+    }
 }
 
 #[derive(Debug)]
@@ -194,6 +274,42 @@ pub struct Lump {
     id: usize,
     pin_ids: Vec<usize>,
     value: Bits,
+}
+
+impl Lump {
+    fn new(id: usize, n: usize, cm: &ComponentManager) -> Self {
+        Lump {
+            id,
+            pin_ids: Vec::new(),
+            value: Bits::new(n),
+        }
+    }
+
+    #[instrument(skip(self))]
+    fn accept_update(&mut self, event: &LumpUpdateEvent, cm: &ComponentManager) {
+        if self.value == event.bits {
+            info!("Value are equal aborting update");
+            return;
+        }
+        self.value = event.bits.clone();
+        for pin_id in &self.pin_ids {
+            if pin_id != &event.sender_pin_id {
+                cm.schedule_pin_update(0, *pin_id, event.bits.clone());
+            }
+        }
+    }
+
+    fn connect(&mut self, pin_id: &usize) {
+        if !self.pin_ids.contains(pin_id) {
+            self.pin_ids.push(*pin_id);
+        }
+    }
+
+    fn disconnect(&mut self, pin_id: &usize) {
+        if let Ok(idx) = self.pin_ids.binary_search(pin_id) {
+            self.pin_ids.push(*pin_id);
+        }
+    }
 }
 
 pub struct GateUpdateData {
@@ -345,7 +461,10 @@ pub fn lib_main() {
     info!("Maybe Works");
     let mut cm = ComponentManager::new();
     let and = AND::new(cm.get_id(), &cm);
+    let lump = Lump::new(0, 1, &cm);
     cm.accept_gate(Box::new(and));
+    cm.accept_lump(lump);
+    cm.connect_pin_to_lump(&4, &0);
     let ids: Vec<usize> = cm.pins.read().unwrap().iter().map(|(_, v)| v.id).collect();
     println!("Pins: {:?}", ids);
     println!("A: {} ", cm.get_pin_value(&2));
